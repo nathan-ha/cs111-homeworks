dfe912f8a1f5e2862b07a190199118aeb0363fd2
Nathan <nathan_ha@outlook.com>
2024-01-29 15:09:13 -0800

    wrote some rsa breaking functions

--- hw2/rsa_hw.cpp
+++ hw2/rsa_hw.cpp
@@ -1,20 +1,49 @@
 #include <iostream>
 using namespace std;
 
-int find_q(int p);
+int solve_for_p(int n);
+int mod_inv(int e, int n);
 
 int main() {
+    const int n = 4453;
+    const int e = 7;
+
     // find p,q
+    int p = solve_for_p(n); // 61
+    int q = n / p; // 73
     // find phi
-    // d
-    
+    int phi = (p - 1)*(q - 1); // 4320
+    // find d
+    int d = mod_inv(e, n); // 3817
+
 }
 
-int find_q(int p) {
-    for (int q = 1; q < p; q++) {
-        if (p % q == 0) {
-            return q;
+// calculates a divisor of n
+int solve_for_p(int n) {
+    for (int p = 2; p < n; p++) {
+        if (n % p == 0) {
+            return p;
         }
     }
     return -1;
+}
+
+// calculates modular inverse (e^-1 (mod n)) by listing multiples
+int mod_inv(int e, int n) {
+    // xe === 1 (mod n) -> ex = ny + 1
+    int x = 1;
+    int y = 1;
+    int lhs = e*x;
+    int rhs = n*y + 1;
+    while (lhs != rhs) {
+        if (lhs < rhs) {
+            x++;
+        }
+        else {
+            y++;
+        }
+        lhs = e*x;
+        rhs = n*y + 1;
+    }
+    return x;
 }
